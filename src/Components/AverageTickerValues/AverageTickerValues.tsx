39ecfd9c995c7e8a5f1e24473f87405f87d3a3cd
Tom Hughes <tomhughes1987@gmail.com>
2022-10-29 18:15:25 +0200

    slow down coinbase WS with diff channel,

--- src/Components/AverageTickerValues/AverageTickerValues.tsx
+++ src/Components/AverageTickerValues/AverageTickerValues.tsx
@@ -12,49 +12,65 @@ export default function AverageTickerValues() {
   //   BitStamp-WS     //
   ///////////////////////
   const [bitstampPriceWs, setBitstampPriceWs] = useState("");
-  const wsBitstamp = new WebSocket("wss://ws.bitstamp.net");
-  useEffect(() => {
-    const apiCall = {
-      event: "bts:subscribe",
-      data: { channel: `live_trades_btcusd` },
-    };
-    wsBitstamp.onopen = (event) => {
-      wsBitstamp.send(JSON.stringify(apiCall));
-    };
-    wsBitstamp.onmessage = function (event) {
-      const json = JSON.parse(event.data);
-      console.log(`[message] Data received from server: ${json}`);
-      try {
-        if ((json.event = "data")) {
-          if (json.data.price) {
-            setBitstampPriceWs(json.data.price);
-          }
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    };
-  }, []);
+  //   const wsBitstamp = new WebSocket("wss://ws.bitstamp.net");
+  //   useEffect(() => {
+  //     const apiCall = {
+  //       event: "bts:subscribe",
+  //       data: { channel: `live_trades_btcusd` },
+  //     };
+  //     wsBitstamp.onopen = (event) => {
+  //       wsBitstamp.send(JSON.stringify(apiCall));
+  //     };
+  //     wsBitstamp.onmessage = function (event) {
+  //       const json = JSON.parse(event.data);
+  //       console.log(`[message] Data received from server: ${json}`);
+  //       try {
+  //         if ((json.event = "data")) {
+  //           if (json.data.price) {
+  //             setBitstampPriceWs(json.data.price);
+  //           }
+  //         }
+  //       } catch (err) {
+  //         console.log(err);
+  //       }
+  //     };
+  //   }, []);
 
   //   ///////////////////////
   //   //   CoinBase-WS     //
   //   ///////////////////////
-  const [coinbasePriceWs, setCoinbasePriceWs] = useState("");
+  const [coinbasePriceWs, setCoinbasePriceWs] = useState(123456789);
 
-  const ws = new WebSocket("wss://ws-feed.exchange.coinbase.com");
+  //   const ws = new WebSocket("wss://ws-feed.prime.coinbase.com");
+
+  const ws = new WebSocket(
+    // GET wss://ws-feed.exchange.coinbase.com
+    // Sec-WebSocket-Extensions: permessage-deflate
+    "wss://ws-feed.exchange.coinbase.com"
+  );
   useEffect(() => {
+    console.log("doing thi");
+
     const apiCall = {
       type: "subscribe",
-      //   product_ids: ["BTC-USD"],
-      channels: [
-        //     "level2",
-        "heartbeat",
-        {
-          name: "ticker",
-          product_ids: ["BTC-USD"],
-        },
-      ],
+
+      product_ids: ["ETH-USD", "BTC-USD"],
+      channels: ["ticker_batch"],
     };
+
+    //   type: "subscribe",
+    //   channel: "level2",
+    //   //   product_ids: ["BTC-USD"],
+    //   channels: [
+    //     // "level2",
+    //     "heartbeat",
+    //     {
+    //       name: "ticker",
+    //       product_ids: ["BTC-USD"],
+    //     },
+    //   ],
+    //   headers: "Sec-WebSocket-Extensions: permessage-deflate",
+    // };
 
     ws.onopen = (event) => {
       ws.send(JSON.stringify(apiCall));
@@ -64,8 +80,11 @@ export default function AverageTickerValues() {
       const json = JSON.parse(event.data);
 
       try {
+        // if (json.price) {
         setCoinbasePriceWs(json.price);
 
+        // }
+        // console.log(json);
         // console.log(json.price);
         // setTimeout(() => {
         // }, );
@@ -88,45 +107,47 @@ export default function AverageTickerValues() {
   //   BitFinex-WS     //
   ///////////////////////
   const [bitfinexPriceWs, setbitfinexPriceWs] = useState(0);
-  const wsBitfinex = new WebSocket("wss://api-pub.bitfinex.com/ws/2");
+  //   const wsBitfinex = new WebSocket("wss://api-pub.bitfinex.com/ws/2");
   //   const w = new ws("wss://api-pub.bitfinex.com/ws/2");
 
-  useEffect(() => {
-    const apiCall = {
-      event: "subscribe",
-      channel: "ticker",
-      symbol: "tBTCUSD",
-    };
+  //   useEffect(() => {
+  //     const apiCall = {
+  //       event: "subscribe",
+  //       channel: "ticker",
+  //       symbol: "tBTCUSD",
+  //     };
 
-    wsBitfinex.onopen = (event) => {
-      wsBitfinex.send(JSON.stringify(apiCall));
-    };
+  //     wsBitfinex.onopen = (event) => {
+  //       wsBitfinex.send(JSON.stringify(apiCall));
+  //     };
 
-    wsBitfinex.onmessage = function (event) {
-      const json = JSON.parse(event.data);
+  //     wsBitfinex.onmessage = function (event) {
+  //       const json = JSON.parse(event.data);
 
-      try {
-        if (json[0] !== undefined) {
-          if (typeof json[1][0] == "number") {
-            console.log(json[1][0]);
-            setbitfinexPriceWs(json[1][0]);
-          }
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    };
-  }, []);
+  //       try {
+  //         if (json[0] !== undefined) {
+  //           if (typeof json[1][0] == "number") {
+  //             console.log(json[1][0]);
+  //             setbitfinexPriceWs(json[1][0]);
+  //           }
+  //         }
+  //       } catch (err) {
+  //         console.log(err);
+  //       }
+  //     };
+  //   }, []);
 
   ////////////////////////////////////////////////////////////////////////
   ////////////////////////////////////////////////////////////
   /////////////////////////////////////////////////////////////////////////
 
+  // if (coinbasePriceWs !=)
+
+  //   if (coinbasePriceWs === "undifined") {
+  //   }
+
   return (
     <>
-      <h1>{counter}</h1>
-      <p>errs=0</p>
-
       <div>AverageTickerValues</div>
       <div>
         <h3>BitStamp:</h3>
@@ -134,13 +155,14 @@ export default function AverageTickerValues() {
       </div>
       <div>
         <h3>CoinBase:</h3>
-        <p>errs=1000000+</p>
 
-        <p>${coinbasePriceWs.slice(0, -3)}</p>
+        {/* <p>${coinbasePriceWs.slice(0, -3)}</p> */}
+        <p>${coinbasePriceWs}</p>
+        {/* <p>${coinbasePriceWs.toFixed()}</p> */}
       </div>
       <div>
         <h3>bitfinex:</h3>
-        <p>errs=0</p>
+
         {/* <p>${bitfinexPriceWs}</p> */}
       </div>
       <div>
